10f046458bea859ef0f5ce95d2fd39ca027135d6
Son-Nguyen48 <sonnt.B16CN303@stu.ptit.edu.vn>
2022-08-19 15:22:56 +0700

    rename file, function, handle when api false

--- js/store.js
+++ js/store.js
@@ -1,57 +1,88 @@
-import fetchTodo from "../js/fetchtodo.js";
 function Store() {
   const listAPI = "https://62f9ae303c4f110faa8b741e.mockapi.io/todoList";
 
   fetch(listAPI)
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("api false");
+    })
     .then((result) => {
       this.listTodo = result;
+    })
+    .catch((error) => {
+      console.log(error);
     });
 
   return {
     listTodo: this.listTodo,
-    addTodo: (data, todo) => {
-      fetch(listAPI, {
+    addTodo: async (data, todo) => {
+      await fetch(listAPI, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
       })
         .then((response) => {
-          return response.json();
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("api fasle");
         })
         .then((posts) => {
-          todo.innerHTML = `<div class="view">
+          todo.insertAdjacentHTML(
+            "afterbegin",
+            `<div class="view">
           <input type="checkbox" class="toggle"/>
           <input type="text" readonly class="content" value="${posts.content}"/>
           <button class="delete">X</button>
-      </div>`;
+      </div>`
+          );
           todo.id = "id_" + posts.id;
+        })
+        .catch((error) => {
+          console.log(error);
         });
     },
-    updateTodo: (data, id, todoContent) => {
-      fetch(listAPI + `/${id}`, {
+    updateTodo: async (data, id, todoContent) => {
+      await fetch(listAPI + `/${id}`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
       })
-        .then((response) => response.json())
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("api false");
+        })
         .then((result) => {
-          console.log(result);
           todoContent.value = result.content;
-          console.log(todoContent.value);
           const readonlyAttribute = document.createAttribute("readonly");
           todoContent.setAttributeNode(readonlyAttribute);
+          todoContent.style.border = "none";
+        })
+        .catch((error) => {
+          console.error(error);
         });
     },
-    deleteTodo: (id, todo) => {
+    deleteTodo: async (id, todo) => {
       console.log(id);
-      fetch(listAPI + `/${id}`, {
+      await fetch(listAPI + `/${id}`, {
         method: "DELETE",
         headers: { "Content-Type": "application/json" }
       })
-        .then((response) => response.json())
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("api false");
+        })
         .then(() => {
           todo.remove();
+        })
+        .catch((error) => {
+          console.error(error);
         });
     }
   };
